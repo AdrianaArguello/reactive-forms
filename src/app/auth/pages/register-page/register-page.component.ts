0c267a601e352c5e310ab4e19e753cf18870a837
Adriana Argüello <70598371+AdrianaArguello@users.noreply.github.com>
2025-07-29 09:31:39 -0400

    added register page form

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,12 +1,31 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+  fb = inject(FormBuilder);
+  formUtils = FormUtils;
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(FormUtils.namePattern)]],
+    email: ['', [Validators.required, Validators.pattern(FormUtils.emailPattern)]],
+    username: ['', [Validators.required, Validators.minLength(6), Validators.pattern(FormUtils.notOnlySpacesPattern)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+  }, {
+    validators: [
+      this.formUtils.isFieldOneEqualsFieldTwo('password', 'confirmPassword')
+    ]
+  });
 
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+    if(this.myForm.invalid) return;
+  }
 }
--- src/app/utils/form-utils.ts
+++ src/app/utils/form-utils.ts
@@ -1,5 +1,9 @@
-import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+import { AbstractControl, FormArray, FormGroup, ValidationErrors } from '@angular/forms';
 export class FormUtils {
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
+
   static getTextError(errors: ValidationErrors): string | null {
   for( const key of Object.keys(errors)){
       switch(key){
@@ -9,6 +13,15 @@ export class FormUtils {
           return `Minimo ${errors['minlength'].requiredLength} caracteres`;
         case 'min':
           return `Minimo ${errors['min'].min} caracteres`;
+        case 'email':
+          return `El valor ingresado no tiene formato de correo`;
+        case 'pattern':
+          if(errors['pattern'].requiredPattern === FormUtils.emailPattern){
+            return `El valor ingresado no tiene formato de correo`;
+          }
+          return 'El valor ingresado no es valido';
+        default:
+          return `Error de validacion no controlado: ${key}`;
       }
     }
     return null;
@@ -39,6 +52,15 @@ export class FormUtils {
     const errors = formArray.controls[index].errors || {};
 
     return FormUtils.getTextError(errors);
+  }
+
+  static isFieldOneEqualsFieldTwo(field1: string, field2: string){
+    return (formGroup: AbstractControl) => {
+      const field1Value = formGroup.get(field1)?.value;
+      const field2Value = formGroup.get(field2)?.value;
+
+      return field1Value === field2Value ? null : { notEquals: true };
+    }
   }
 
 }
